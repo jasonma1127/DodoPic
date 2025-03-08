ae4e4d8fcf1f9fcb4236593e634d2e1b4b591d0e
jasonma1127 <jasonma1127@gmail.com>
2025-03-09 00:29:08 +0800

    Create Frame, Camera and Fashion component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Frame from './components/Frame';
+import Camera from './components/Camera';
+import Fashion from './components/Fashion';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [layout, setLayout] = useState('0');
+    const [images, setImages] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleLayoutChange = (layout) => {
+        const size = layout.split("x");
+        const total = size[0] * size[1];
+        setLayout(total);
+    };
+
+    const handleImagesCaptured = (capturedImages) => {
+        setImages(capturedImages);
+    };
+
+    return (
+        <div>
+            <h1>DodoPic</h1>
+            <Frame onLayoutChange={handleLayoutChange} />
+            <h3>{layout}</h3>
+            <Camera layout={layout} onImagesCaptured={handleImagesCaptured} />
+            <Fashion images={images} />
+        </div>
+
+    );
 }
 
-export default App
+export default App;
